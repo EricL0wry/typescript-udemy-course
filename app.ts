b0b52d726d9443b507144d6bd0e797c995680ae8
Eric Lowry <ericmichaellowry@gmail.com>
2020-07-07 13:26:14 -0700

    Added object type example

--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
 const person = {
   name: 'Eric',
   age: 36
 };
 
-console.log(person);
+console.log(person.name);
--- basics.ts
+++ basics.ts
@@ -15,6 +15,5 @@ number1 = 5;
 const number2 = 2.8;
 const printResult = true;
 let resultPhrase = 'Result is: ';
-resultPhrase  = 0;
 
 const result = add(number1, number2, printResult, resultPhrase);
