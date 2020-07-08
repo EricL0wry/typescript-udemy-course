2e78f3018efbf2b1febd680dfe3485d158ebe529
Eric Lowry <ericmichaellowry@gmail.com>
2020-07-08 11:28:58 -0700

    Created new basic Department class

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("hello");
+class Department {
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  name: string;
+
+}
+
+const accounting = new Department('Accounting');
+
+console.log(accounting);
