25192963608505a8204bad82ea87942031ba200a
Eric Lowry <ericmichaellowry@gmail.com>
2020-07-08 14:54:09 -0700

    Renamed app file as classes to archive class examples. Created new app file. Added interface example to app file

--- src/app.ts
+++ src/app.ts
@@ -1,125 +1,18 @@
-abstract class Department {
-  static fiscalYear = 2020;
-  // private id: string;
-  // private name: string;
-  protected employees: string[] = [];
+interface Person {
+  name: string;
+  age: number;
 
-  constructor(protected readonly id: string, private name: string) {
-    // this.name = name;
-    // this.id = id;
-  }
-
-  static createEmployee(name: string) {
-    return {name: name};
-  }
-
-  abstract describe(this: Department): void;
-
-  addEmployee(employee: string) {
-    this.employees.push(employee);
-  }
-
-  printEmployeeInformation() {
-    console.log(this.employees.length);
-    console.log(this.employees);
-  }
+  greet(phrase: string): void;
 }
 
-class ITDepartment extends Department {
-  admins: string[]
-  constructor(id: string, admins: string[]) {
-    super(id, 'IT');
-    this.admins = admins;
+let user1: Person;
+
+user1 = {
+  name: 'Eric',
+  age: 36,
+  greet(phrase: string) {
+    console.log(phrase + ' ' + this.name);
   }
+};
 
-  describe() {
-    console.log('IT Department - ID: ' + this.id);
-  }
-}
-
-class AccountingDepartment extends Department {
-  private lastReport: string;
-  private static instance: AccountingDepartment;
-
-  get mostRecentReport() {
-    if (this.lastReport) {
-      return this.lastReport;
-    }
-    throw new Error('No report found');
-  }
-
-  set mostRecentReport(value: string) {
-    if (!value) {
-      throw new Error('Please pass in a valid value');
-    }
-    this.addReport(value);
-  }
-  
-  private constructor(id: string, private reports: string[]) {
-    super(id, 'Accounting');
-    this.lastReport = reports[0];
-  }
-
-  static getInstance() {
-    if (AccountingDepartment.instance) {
-      return this.instance
-    }
-    this.instance = new AccountingDepartment('d2', []);
-    return this.instance;
-  }
-
-  describe() {
-    console.log('Accounting Department - ID: ' + this.id)
-  }
-
-  addEmployee(name: string) {
-    if (name === 'Max') return;
-    this.employees.push(name);
-  }
-
-  addReport(text: string) {
-    this.reports.push(text);
-    this.lastReport = text;
-  }
-
-  printReports() {
-    console.log(this.reports);
-  }
-}
-
-const employee1 = Department.createEmployee('Max');
-console.log(employee1, Department.fiscalYear);
-
-const it = new ITDepartment('d1', ['Max']);
-
-
-
-it.addEmployee('Max');
-it.addEmployee('Manu');
-
-// accounting.employees[2] = 'Anna';  // will cause TS error because the employees property is set to private
-
-it.describe();
-// accounting.name = 'NEW NAME';
-it.printEmployeeInformation();
-
-console.log(it);
-
-// const accounting = new AccountingDepartment('d2', []);
-const accounting = AccountingDepartment.getInstance();
-
-accounting.mostRecentReport = 'This is a report';
-accounting.addReport('Something went wrong...');
-console.log(accounting.mostRecentReport);
-
-accounting.addEmployee('Max');
-accounting.addEmployee('Manu');
-
-// accounting.printReports();
-// accounting.printEmployeeInformation();
-
-accounting.describe();
-
-// const accountingCopy = { name: 'DUMMY', describe: it.describe };
-
-// accountingCopy.describe(); // Throws an error because the structure of accountingCopy doesn't match that of accounting
+user1.greet('Hi there, I am');
