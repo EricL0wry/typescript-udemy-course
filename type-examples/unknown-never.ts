713e7a9f68513a645dc753ba7ce3a23ff805d055
Eric Lowry <ericmichaellowry@gmail.com>
2020-07-08 11:15:09 -0700

    Moved example files to new directory to prepare for next section

---
+++ type-examples/unknown-never.ts
@@ -0,0 +1,12 @@
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+// userName = userInput;
+
+function generateError(message: string, code: number): never {
+  throw {message: message, errorCode: code};
+}
+
+generateError('An error occurred!', 500);
